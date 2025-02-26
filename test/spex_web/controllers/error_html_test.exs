6c75746adc08b1b53c0e0aa434fd6a76dd8c909b
Ross Hammermeister <3009978+RossHammer@users.noreply.github.com>
2025-02-26 10:42:47 -0700

    Initial commit

---
+++ test/spex_web/controllers/error_html_test.exs
@@ -0,0 +1,14 @@
+defmodule SpexWeb.ErrorHTMLTest do
+  use SpexWeb.ConnCase, async: true
+
+  # Bring render_to_string/4 for testing custom views
+  import Phoenix.Template
+
+  test "renders 404.html" do
+    assert render_to_string(SpexWeb.ErrorHTML, "404", "html", []) == "Not Found"
+  end
+
+  test "renders 500.html" do
+    assert render_to_string(SpexWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
+  end
+end
